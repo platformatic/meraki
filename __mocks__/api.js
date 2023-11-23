22b4d84ee047fa2c11928f5891c769afd06aedfc
Antonio Sonis <anthonysonis@gmail.com>
2023-11-23 09:18:00 +0100

    feat: updating mocks
    Signed-off-by: Antonio Sonis <anthonysonis@gmail.com>

--- __mocks__/api.js
+++ __mocks__/api.js
@@ -1,23 +1,5 @@
-const nameList = [
-  'Time - Past - Future - Dev',
-  'Fly - Flying - Soar - Soaring',
-  'Power - Falling',
-  'Sharp - Dead - Mew - Chuckle - Bubba',
-  'Time', 'Past', 'Future', 'Dev',
-  'Fly', 'Flying', 'Soar', 'Soaring',
-  'Power', 'Falling',
-  'Legacy', 'Sharp', 'Dead', 'Mew', 'Chuckle', 'Bubba',
-  'Bubble',
-  'Sandwich',
-  'Smasher - Extreme - Multi',
-  'Smasher', 'Extreme', 'Multi',
-  'Universe',
-  'Ultimate',
-  'Death',
-  'Ready - Monkey',
-  'Ready', 'Monkey',
-  'Paradox'
-]
+import mockedPlugins from './plugins.json'
+import mockedTemplates from './templates.json'
 
 const envList = [
   'MENDACITY',
@@ -34,39 +16,11 @@ const envList = [
   'PERPETRATE'
 ]
 
-// TODO: replace with the actual calls
-export const getTemplates = async () => {
-  const howMany = Math.floor(Math.random() * (nameList.length - 10))
-  // const howMany = 26
-  const nameArray = ['Platformatic service']
-  let name
-  while (nameArray.length < howMany) {
-    name = nameList[Math.floor(Math.random() * nameList.length)]
-    if (!nameArray.includes(name)) {
-      nameArray.push(name)
-    }
-  }
+export const getTemplates = async () => mockedTemplates
 
-  return nameArray.map((name, index) => ({
-    name,
-    platformaticService: index === 0,
-    env: Array.from(new Array(Math.floor(Math.random() * envList.length)).keys()).map(() => envList[Math.floor(Math.random() * envList.length)])
-  }))
-}
-
-// TODO: replace with the actual calls
 export const getPlugins = async () => {
-  const howMany = Math.floor(Math.random() * (nameList.length - 10))
-  const nameArray = []
-  let name
-  while (nameArray.length < howMany) {
-    name = nameList[Math.floor(Math.random() * nameList.length)]
-    if (!nameArray.includes(name)) {
-      nameArray.push(name)
-    }
-  }
-
-  return nameArray.map((name, index) => ({
-    name
+  return mockedPlugins.map(plugin => ({
+    ...plugin,
+    envVars: Array.from(new Array(Math.floor(Math.random() * envList.length)).keys()).map(() => envList[Math.floor(Math.random() * envList.length)])
   }))
 }
--- src/main/client.js
+++ src/main/client.js
@@ -1,23 +1,5 @@
-const nameList = [
-  'Time - Past - Future - Dev',
-  'Fly - Flying - Soar - Soaring',
-  'Power - Falling',
-  'Sharp - Dead - Mew - Chuckle - Bubba',
-  'Time', 'Past', 'Future', 'Dev',
-  'Fly', 'Flying', 'Soar', 'Soaring',
-  'Power', 'Falling',
-  'Legacy', 'Sharp', 'Dead', 'Mew', 'Chuckle', 'Bubba',
-  'Bubble',
-  'Sandwich',
-  'Smasher - Extreme - Multi',
-  'Smasher', 'Extreme', 'Multi',
-  'Universe',
-  'Ultimate',
-  'Death',
-  'Ready - Monkey',
-  'Ready', 'Monkey',
-  'Paradox'
-]
+import mockedPlugins from '../../__mocks__/plugins.json'
+import mockedTemplates from '../../__mocks__/templates.json'
 
 const envList = [
   'MENDACITY',
@@ -35,40 +17,12 @@ const envList = [
 ]
 
 // TODO: replace with the actual calls
-export const getTemplates = async () => {
-  const howMany = Math.floor(Math.random() * (nameList.length - 10))
-  // const howMany = 26
-  const nameArray = ['Platformatic service']
-  let name
-  while (nameArray.length < howMany) {
-    name = nameList[Math.floor(Math.random() * nameList.length)]
-    if (!nameArray.includes(name)) {
-      nameArray.push(name)
-    }
-  }
-
-  return nameArray.map((name, index) => ({
-    id: index + 1,
-    name,
-    platformaticService: index === 0,
-    env: Array.from(new Array(Math.floor(Math.random() * envList.length)).keys()).map(() => envList[Math.floor(Math.random() * envList.length)])
-  }))
-}
+export const getTemplates = async () => mockedTemplates
 
 // TODO: replace with the actual calls
 export const getPlugins = async () => {
-  const howMany = Math.floor(Math.random() * (nameList.length - 10))
-  const nameArray = []
-  let name
-  while (nameArray.length < howMany) {
-    name = nameList[Math.floor(Math.random() * nameList.length)]
-    if (!nameArray.includes(name)) {
-      nameArray.push(name)
-    }
-  }
-
-  return nameArray.map((name, index) => ({
-    id: index + 1,
-    name
+  return mockedPlugins.map(mockedPlugin => ({
+    ...mockedPlugin,
+    envVars: Array.from(new Array(Math.floor(Math.random() * envList.length)).keys()).map(() => envList[Math.floor(Math.random() * envList.length)])
   }))
 }
