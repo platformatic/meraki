f74e71df6c6db480e51a236f21a8d8cca4ba6f9d
marcopiraccini <marco.piraccini@gmail.com>
2023-11-27 17:41:41 +0100

    install templates and plugins

--- src/main/oss_templates.mjs
+++ src/main/oss_templates.mjs
@@ -1,7 +1,7 @@
 const templates = [{
   orgId: 'platformatic',
   orgName: 'Platformatic',
-  name: 'Platformatic Service',
+  name: '@platformatic/service',
   description: 'A Platformatic Service is an HTTP server based on Fastify that allows developers to build robust APIs with Node.js',
   author: 'Platformatic',
   homepage: 'https://platformatic.dev',
@@ -11,7 +11,7 @@ const templates = [{
 {
   orgId: 'platformatic',
   orgName: 'Platformatic',
-  name: 'Platformatic DB',
+  name: '@platformatic/db',
   description: 'Platformatic DB can expose a SQL database by dynamically mapping it to REST/OpenAPI and GraphQL endpoints. It supports a limited subset of the SQL query language, but also allows developers to add their own custom routes and resolvers',
   author: 'Platformatic',
   homepage: 'https://platformatic.dev',
@@ -20,7 +20,7 @@ const templates = [{
 {
   orgId: 'platformatic',
   orgName: 'Platformatic',
-  name: 'Platformatic Composer',
+  name: '@platformatic/composer',
   description: 'Platformatic Composer is an HTTP server that automatically aggregates multiple services APIs into a single API.',
   author: 'Platformatic',
   homepage: 'https://platformatic.dev',
