3269be4eaf2c88e25f3877620aa503bbce4d31f1
marcopiraccini <marco.piraccini@gmail.com>
2023-11-27 14:59:59 +0100

    env variables and mocking fixes

--- src/renderer/src/api.js
+++ src/renderer/src/api.js
@@ -1,17 +1,9 @@
 import { getTemplates, getPlugins } from '~/../../../__mocks__/api'
 
-const mockUse = import.meta.env.RENDERER_VITE_USE_MOCKS === 'true'
-
 export const getApiTemplates = async () => {
-  if (mockUse) {
-    return await getTemplates()
-  }
   return await window.api.getTemplates()
 }
 
 export const getApiPlugins = async () => {
-  if (mockUse) {
-    return await getPlugins()
-  }
   return await window.api.getPlugins()
 }
