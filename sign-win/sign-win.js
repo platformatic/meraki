b022f2558661239688edf3ed85bfd1c7d35b1e34
marcopiraccini <marco.piraccini@gmail.com>
2023-12-03 13:03:45 +0100

    test with win gh action

--- sign-win/sign-win.js
+++ sign-win/sign-win.js
@@ -1,11 +1,10 @@
-import { execa } from 'execa'
-
 // This is a callback that can be used to sign the executable on Windows.
 // See: https://www.electron.build/configuration/win
 // We are using this callback instead of the electron-builder built-in mechanism
 // because we want to use the DigiCert signing tool to sign the executable.
 // See also: https://docs.digicert.com/en/digicert-keylocker/sign-with-digicert-signing-tools/sign-with-smctl.html
 exports.default = async function (configuration) {
+  const { execa } = await import('execa')
   try {
     console.log('@@ Signing for windows', configuration.path)
     const execPath = configuration.path
