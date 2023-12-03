bef933bc918e8ee1914036fa5002a846a193a871
marcopiraccini <marco.piraccini@gmail.com>
2023-12-03 13:24:05 +0100

    test with win gh action

--- sign-win/sign-win.js
+++ sign-win/sign-win.js
@@ -7,7 +7,7 @@ exports.default = async function (configuration) {
   const { execa } = await import('execa')
   console.log('@@ Signing for windows', configuration.path)
   const execPath = configuration.path
-  const { all, exitCode } = await execa('smctl', [
+  const { stdout, exitCode } = await execa('smctl', [
     'sign',
     '--fingerprint',
     'f5f3e92cc99cc0169228d81ef65ba16ea680fe0a',
@@ -17,7 +17,7 @@ exports.default = async function (configuration) {
   // `smctl` returns exit code 0 even if the signing fails :(. )
   // So we need to check the output for errors.
   // See also: https://docs.digicert.com/en/digicert-keylocker/sign-with-digicert-signing-tools/sign-with-smctl.html
-  if (all.includes('FAILED')) {
+  if (stdout.includes('FAILED')) {
     console.error('Signing failed:', all, exitCode)
     throw new Error(stdout)
   }
