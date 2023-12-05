b42f050ccc1590efd1a8ffb69de5a8850c66d6ad
Marco Piraccini <marco.piraccini@gmail.com>
2023-12-05 06:23:36 +0100

    sgn-win without execa

--- sign-win.js
+++ sign-win.js
@@ -1,3 +1,5 @@
+const { spawnSync } = require('child_process')
+
 // This is a callback that can be used to sign the executable on Windows.
 // See: https://www.electron.build/configuration/win
 // We are using this callback instead of the electron-builder built-in mechanism
@@ -7,18 +9,28 @@ exports.default = async function (configuration) {
   const { execa } = await import('execa')
   console.log('Signing: ', configuration.path)
   const execPath = configuration.path
-  const { stdout, exitCode } = await execa('smctl', [
+
+  const child = spawnSync('smctl', [
     'sign',
     '--fingerprint',
     process.env.DIGICERT_FINGERPRINT,
     '--input',
     execPath
-  ])
+  ],  { encoding: 'utf-8' })
+
+  if (child.error) {
+    console.error('Signing failed:', child.error)
+    throw new Error(child.error)
+  }
+  
+  console.log(child.stdout)
+
   // `smctl` returns exit code 0 even if the signing fails :(. )
   // So we need to check the output for errors.
   // See also: https://docs.digicert.com/en/digicert-keylocker/sign-with-digicert-signing-tools/sign-with-smctl.html
-  if (stdout.includes('FAILED')) {
-    console.error('Signing failed:', stdout, exitCode)
-    throw new Error(stdout)
+  if (child.stdout.includes('FAILED')) {
+    console.error('Signing failed:', child.stdout)
+    throw new Error(child.stdout)
   }
+  console.log('Signed ', configuration.path)
 }
