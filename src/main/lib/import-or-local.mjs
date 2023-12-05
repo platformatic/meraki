48eeb25ecb53bd7c93541fe25d2ef6ecb0a96a14
marcopiraccini <marco.piraccini@gmail.com>
2023-12-05 12:05:34 +0100

    fix to find npm in OSx and linu when deb

--- src/main/lib/import-or-local.mjs
+++ src/main/lib/import-or-local.mjs
@@ -2,6 +2,7 @@ import { pathToFileURL } from 'node:url'
 import path from 'node:path'
 import split from 'split2'
 import { createRequire } from 'module'
+import { spawn } from 'node:child_process'
 import execa from 'execa'
 
 async function importOrLocal ({ pkgManager, projectDir, pkg, logger }) {
@@ -17,21 +18,65 @@ async function importOrLocal ({ pkgManager, projectDir, pkg, logger }) {
       return await import(pathToFileURL(fileToImport))
     } catch (err) {}
 
-    logger.info({ name: pkg, path: projectDir }, `Installing ${pkg} on ${projectDir}...`)
-    const child = execa(pkgManager, ['install', pkg], { cwd: projectDir })
+    logger.info(`Installing ${pkg} on ${projectDir}...`)
 
-    child.stdout.pipe(split()).on('data', (line) => {
-      logger.info(line)
-    })
+    if (process.platform === 'win32') {
+      const child = execa(pkgManager, ['install', pkg], { cwd: projectDir })
 
-    child.stderr.pipe(split()).on('data', (line) => {
-      logger.error(line)
-    })
-    await child
+      child.stdout.pipe(split()).on('data', (line) => {
+        logger.info(line)
+      })
 
-    logger.info({ name: pkg, path: projectDir }, 'Installed!')
-    const fileToImport = _require.resolve(pkg)
-    return await import(pathToFileURL(fileToImport))
+      child.stderr.pipe(split()).on('data', (line) => {
+        logger.error(line)
+      })
+      await child
+
+      logger.info({ name: pkg, path: projectDir }, 'Installed!')
+      const fileToImport = _require.resolve(pkg)
+      return await import(pathToFileURL(fileToImport))
+    } else {
+      const currentShell = process.env.SHELL || '/bin/zsh'
+      let sourceFile = '~/.zshrc'
+      if (currentShell === '/bin/bash') {
+        sourceFile = '~./bashrc'
+      }
+
+      // OSx and linux
+      const child = spawn(process.env.SHELL, ['-c', `. ${sourceFile}; npm i ${pkg}`], {
+        cwd: projectDir
+      })
+
+      child.stdout.setEncoding('utf8')
+      child.stdout.on('data', (message) => {
+        logger.info(message)
+        console.log(message)
+      })
+
+      child.stderr.setEncoding('utf8')
+      child.stderr.on('data', (message) => {
+        logger.info(message)
+        console.log(message)
+      })
+
+      child.on('error', async (err) => {
+        logger.error(err)
+        console.log(err)
+      })
+
+      return new Promise((resolve, reject) => {
+        child.on('exit', async () => {
+          try {
+            logger.info('Installed!')
+            const fileToImport = _require.resolve(pkg)
+            const pathToPkg = await import(pathToFileURL(fileToImport))
+            resolve(pathToPkg)
+          } catch (err) {
+            reject(err)
+          }
+        })
+      })
+    }
   }
 }
 
