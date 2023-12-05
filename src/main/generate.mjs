b0fcb52ee50ba12c89aee6eb4265b6efe4ef36f7
marcopiraccini <marco.piraccini@gmail.com>
2023-12-05 13:04:17 +0100

    tests fixups

--- src/main/generate.mjs
+++ src/main/generate.mjs
@@ -60,7 +60,7 @@ export const prepareFolder = async (path, tempNames, logger, appName = 'appName'
 export const createApp = async (dir, { projectName, services, entrypoint, port, logLevel, typescript, createGitHubRepository, installGitHubAction }, logger) => {
   const projectDir = join(dir, projectName)
 
-  mkdirp.sync(projectDir)
+  await mkdirp.sync(projectDir)
 
   if (!services || services.length === 0) {
     logger.error('No services to create')
