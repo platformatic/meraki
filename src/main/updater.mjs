7ce75733cd751b074e31f0dac0a274f020b7c259
marcopiraccini <marco.piraccini@gmail.com>
2023-12-11 15:06:53 +0100

    Automatic updater
    Signed-off-by: marcopiraccini <marco.piraccini@gmail.com>

--- src/main/updater.mjs
+++ src/main/updater.mjs
@@ -4,30 +4,32 @@ import log from 'electron-log'
 
 let updater
 autoUpdater.autoDownload = false
+log.transports.console.level = 'info'
 autoUpdater.logger = log
-autoUpdater.logger.transports.file.level = 'info'
 
 autoUpdater.on('error', (error) => {
+  log.error(error)
   dialog.showErrorBox('Error: ', error == null ? 'unknown' : (error.stack || error).toString())
 })
 
-autoUpdater.on('update-available', () => {
-  dialog.showMessageBox({
+autoUpdater.on('update-available', async () => {
+  log.info('Update available')
+  const buttonIndex = await dialog.showMessageBox({
     type: 'info',
     title: 'Found Meraki Update',
     message: 'Found update, do you want update now?',
     buttons: ['Sure', 'No']
-  }).then((buttonIndex) => {
-    if (buttonIndex === 0) {
-      autoUpdater.downloadUpdate()
-    } else {
-      updater.enabled = true
-      updater = null
-    }
   })
+  if (buttonIndex === 0) {
+    autoUpdater.downloadUpdate()
+  } else {
+    updater.enabled = true
+    updater = null
+  }
 })
 
 autoUpdater.on('update-not-available', () => {
+  log.info('Update not available')
   dialog.showMessageBox({
     title: 'No Updates',
     message: 'Current version is up-to-date.'
@@ -36,13 +38,13 @@ autoUpdater.on('update-not-available', () => {
   updater = null
 })
 
-autoUpdater.on('update-downloaded', () => {
-  dialog.showMessageBox({
+autoUpdater.on('update-downloaded', async () => {
+  log.info('Update downloaded')
+  await dialog.showMessageBox({
     title: 'Install Updates',
     message: 'Updates downloaded, application will be quit for update...'
-  }).then(() => {
-    setImmediate(() => autoUpdater.quitAndInstall())
   })
+  autoUpdater.quitAndInstall()
 })
 
 // export this to MenuItem click callback
