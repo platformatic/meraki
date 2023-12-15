728311b94a4dddff1dfedb1fd1fa879d0eeda022
Antonio Sonis <anthonysonis@gmail.com>
2023-12-15 15:23:42 +0100

    fix: meraki logo jsx
    Signed-off-by: Antonio Sonis <anthonysonis@gmail.com>

--- src/renderer/src/components/ui/MerakiLogo.jsx
+++ src/renderer/src/components/ui/MerakiLogo.jsx
@@ -2,9 +2,9 @@
 function MerakiLogo () {
   return (
     <svg width='34' height='35' viewBox='0 0 34 35' fill='none' xmlns='http://www.w3.org/2000/svg'>
-      <path d='M22.2516 22.808C28.1303 22.808 32.8959 18.0424 32.8959 12.1637C32.8959 6.28511 28.1303 1.51953 22.2516 1.51953C16.373 1.51953 11.6074 6.28511 11.6074 12.1637C11.6074 18.0424 16.373 22.808 22.2516 22.808Z' stroke='#676767' stroke-width='2' stroke-linecap='round' stroke-linejoin='round' />
-      <path d='M1 12.1663V33.5L19.6102 22.8325L1 12.1663Z' stroke='#676767' stroke-width='2' stroke-linecap='round' stroke-linejoin='round' />
-      <path d='M22.3337 1.5H1V22.8337H22.3337V1.5Z' stroke='white' stroke-width='2' stroke-linecap='round' stroke-linejoin='round' />
+      <path d='M22.2516 22.808C28.1303 22.808 32.8959 18.0424 32.8959 12.1637C32.8959 6.28511 28.1303 1.51953 22.2516 1.51953C16.373 1.51953 11.6074 6.28511 11.6074 12.1637C11.6074 18.0424 16.373 22.808 22.2516 22.808Z' stroke='#676767' strokeWidth={2} strokeLinecap='round' strokeLinejoin='round' />
+      <path d='M1 12.1663V33.5L19.6102 22.8325L1 12.1663Z' stroke='#676767' strokeWidth={2} strokeLinecap='round' strokeLinejoin='round' />
+      <path d='M22.3337 1.5H1V22.8337H22.3337V1.5Z' stroke='white' strokeWidth={2} strokeLinecap='round' strokeLinejoin='round' />
     </svg>
   )
 }
