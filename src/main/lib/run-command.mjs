0adfd1216797232e2f98143fc08e476060fd504d
marcopiraccini <marco.piraccini@gmail.com>
2024-01-22 11:31:40 +0100

    test mac comment
    Signed-off-by: marcopiraccini <marco.piraccini@gmail.com>

--- src/main/lib/run-command.mjs
+++ src/main/lib/run-command.mjs
@@ -14,6 +14,11 @@ function runCommand (command, args, options) {
     sourceFile = '~./bashrc'
   }
 
+  // TODO: verificare se non fallisce. Se fallsice, provare con  
+  // /usr/local/bin/npm e /usr/bin/npm (senza source, il cmando diventa 
+  // `/usr/local/bin/npm install`.
+  // Questo pero' prevede che questa funzione SAPPIA che sta eseguendo `npm`, quindi
+  // 
   return execa(currentShell, [`"-c . ${sourceFile}; ${command} ${args.join(' ')}"`], options)
 }
 
