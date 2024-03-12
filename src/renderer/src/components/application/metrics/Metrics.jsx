87d553660f4d15479b0dc8487cf44bedea84be46
Antonio Sonis <anthonysonis@gmail.com>
2024-03-12 11:54:04 +0100

    fix: updating metrics layout
    Signed-off-by: Antonio Sonis <anthonysonis@gmail.com>

--- src/renderer/src/components/application/metrics/Metrics.jsx
+++ src/renderer/src/components/application/metrics/Metrics.jsx
@@ -1,25 +1,55 @@
 'use strict'
-import React from 'react'
+import React, { useEffect } from 'react'
 import PropTypes from 'prop-types'
-// import { RICH_BLACK, WHITE, MARGIN_0, OPACITY_30, TRANSPARENT, DULLS_BACKGROUND_COLOR, ANTI_FLASH_WHITE } from '@platformatic/ui-components/src/components/constants'
+import { MEDIUM, WHITE, OPACITY_30, TRANSPARENT } from '@platformatic/ui-components/src/components/constants'
 import styles from './Metrics.module.css'
-// import typographyStyles from '~/styles/Typography.module.css'
+import typographyStyles from '~/styles/Typography.module.css'
 import commonStyles from '~/styles/CommonStyles.module.css'
-// import { Button, HorizontalSeparator, VerticalSeparator } from '@platformatic/ui-components'
-import Title from '~/components/ui/Title'
+import { BorderedBox } from '@platformatic/ui-components'
+import Icons from '@platformatic/ui-components/src/components/icons'
+import useStackablesStore from '~/useStackablesStore'
+import { APPLICATION_PAGE_METRICS } from '~/ui-constants'
 
-const Metrics = React.forwardRef((_props, ref) => {
+const Metrics = React.forwardRef(({ applicationSelected }, ref) => {
+  const globalState = useStackablesStore()
+  const { setNavigation, setCurrentPage } = globalState
+
+  useEffect(() => {
+    if (applicationSelected.id) {
+      console.log('here the id', applicationSelected.id)
+    }
+  }, [applicationSelected.id])
+
+  useEffect(() => {
+    setNavigation({
+      label: 'Metrics',
+      handleClick: () => {
+        setCurrentPage(APPLICATION_PAGE_METRICS)
+      },
+      key: APPLICATION_PAGE_METRICS,
+      page: APPLICATION_PAGE_METRICS
+    }, 2)
+  }, [])
+
   return (
     <div className={styles.container} ref={ref}>
       <div className={styles.content}>
-        <div className={`${commonStyles.largeFlexBlock} ${commonStyles.fullWidth}`}>
+        <div className={`${commonStyles.largeFlexBlock40} ${commonStyles.fullWidth}`}>
           <div className={commonStyles.mediumFlexBlock}>
-            <Title
-              title='Metrics'
-              iconName='StackablesTemplateIcon'
-              dataAttrName='cy'
-              dataAttrValue='modal-title'
-            />
+            <div className={`${commonStyles.mediumFlexRow} ${commonStyles.fullWidth} ${commonStyles.itemsCenter}`}>
+              <Icons.MetricsIcon color={WHITE} size={MEDIUM} />
+              <h2 className={`${typographyStyles.desktopHeadline2} ${typographyStyles.textWhite}`}>Metrics</h2>
+              <p className={`${typographyStyles.desktopBodySmall} ${typographyStyles.textWhite} ${typographyStyles.opacity70}`}>(Last 5 minutes)</p>
+            </div>
+          </div>
+          <div className={`${commonStyles.mediumFlexBlock} ${commonStyles.fullWidth}`}>
+            <BorderedBox color={WHITE} borderColorOpacity={OPACITY_30} backgroundColor={TRANSPARENT} classes={styles.boxMetricContainer}>
+              <p className=''>Metric 1 for {applicationSelected.id}</p>
+            </BorderedBox>
+
+            <BorderedBox color={WHITE} borderColorOpacity={OPACITY_30} backgroundColor={TRANSPARENT} classes={styles.boxMetricContainer}>
+              <p className=''>Metric 2 for {applicationSelected.id} </p>
+            </BorderedBox>
           </div>
         </div>
       </div>
@@ -29,22 +59,13 @@ const Metrics = React.forwardRef((_props, ref) => {
 
 Metrics.propTypes = {
   /**
-   * name
+   * applicationSelected
     */
-  name: PropTypes.string,
-  /**
-   * onClickEdit
-   */
-  onCloseModal: PropTypes.func,
-  /**
-   * onClickRemove
-   */
-  onClickConfirm: PropTypes.func
+  applicationSelected: PropTypes.object
 }
 
 Metrics.defaultProps = {
-  onCloseModal: () => {},
-  onClickConfirm: () => {}
+  applicationSelected: {}
 }
 
 export default Metrics
