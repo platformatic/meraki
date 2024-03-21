783f3c689e6d19f5945fcb47854fdf2bc1b75d14
Antonio Sonis <anthonysonis@gmail.com>
2024-03-21 19:47:20 +0100

    fix: updating stuff
    Signed-off-by: Antonio Sonis <anthonysonis@gmail.com>

--- src/renderer/src/components/application/overview/Overview.jsx
+++ src/renderer/src/components/application/overview/Overview.jsx
@@ -4,20 +4,34 @@ import PropTypes from 'prop-types'
 import styles from './Overview.module.css'
 import { HorizontalSeparator } from '@platformatic/ui-components'
 import useStackablesStore from '~/useStackablesStore'
-import { APPLICATION_PAGE_OVERVIEW /*, APPLICATION_PATH */ } from '~/ui-constants'
+import { APPLICATION_PAGE_OVERVIEW, PAGE_ALL_APPS, HOME_PATH } from '~/ui-constants'
 import TopContent from './TopContent'
 import OverviewSection from './OverviewSection'
 import ServicesSection from './ServicesSection'
 import { MARGIN_0, OPACITY_30, WHITE } from '@platformatic/ui-components/src/components/constants'
 import ErrorComponent from '~/components/screens/ErrorComponent'
+import { useNavigate } from 'react-router-dom'
 
 const Overview = React.forwardRef(({ applicationSelected, onClickEditApplication }, ref) => {
   const globalState = useStackablesStore()
-  const { setNavigation, setCurrentPage } = globalState
+  const { breadCrumbs, setNavigation, setCurrentPage, setReloadApplications } = globalState
   const [showErrorComponent, setShowErrorComponent] = useState(false)
   const [error, setError] = useState(false)
+  const navigate = useNavigate()
 
   useEffect(() => {
+    if (breadCrumbs.length === 0) {
+      setNavigation({
+        label: 'All Apps',
+        handleClick: () => {
+          navigate(HOME_PATH)
+          setCurrentPage(PAGE_ALL_APPS)
+          setReloadApplications(true)
+        },
+        key: PAGE_ALL_APPS,
+        page: PAGE_ALL_APPS
+      })
+    }
     setNavigation({
       label: applicationSelected.name,
       handleClick: () => {
