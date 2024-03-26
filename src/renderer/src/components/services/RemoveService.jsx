2c453476a53cf9cb999c09511d23cb5423e89c89
Antonio Sonis <anthonysonis@gmail.com>
2024-03-26 08:12:27 +0100

    fix: updating delete modal
    Signed-off-by: Antonio Sonis <anthonysonis@gmail.com>

--- src/renderer/src/components/services/RemoveService.jsx
+++ src/renderer/src/components/services/RemoveService.jsx
@@ -1,7 +1,7 @@
 'use strict'
 import React from 'react'
 import PropTypes from 'prop-types'
-import { ERROR_RED, MARGIN_0, OPACITY_30, TRANSPARENT, WHITE } from '@platformatic/ui-components/src/components/constants'
+import { DULLS_BACKGROUND_COLOR, ERROR_RED, MARGIN_0, OPACITY_30, TRANSPARENT, WHITE } from '@platformatic/ui-components/src/components/constants'
 import typographyStyles from '~/styles/Typography.module.css'
 import commonStyles from '~/styles/CommonStyles.module.css'
 import { Button, HorizontalSeparator } from '@platformatic/ui-components'
@@ -16,19 +16,24 @@ function RemoveService ({ name, onClickCancel, onClickConfirm }) {
       <div className={`${commonStyles.smallFlexRow} ${commonStyles.fullWidth} ${commonStyles.justifyBetween}`}>
         <Button
           type='button'
+          textClass={typographyStyles.desktopBody}
           paddingClass={commonStyles.buttonPadding}
           label='Cancel'
           onClick={() => onClickCancel()}
           color={WHITE}
           backgroundColor={TRANSPARENT}
+          
         />
         <Button
           type='button'
+          textClass={typographyStyles.desktopBody}
           paddingClass={commonStyles.buttonPadding}
           label='Delete Service'
           onClick={() => onClickConfirm()}
-          color={ERROR_RED}
-          backgroundColor={TRANSPARENT}
+          color={WHITE}
+          backgroundColor={ERROR_RED}
+          hoverEffect={DULLS_BACKGROUND_COLOR}
+          bordered={false}
         />
       </div>
     </div>
