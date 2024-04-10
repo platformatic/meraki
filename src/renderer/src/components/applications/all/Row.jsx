72a7ae72fc3356acfd6ca68342498fe40dec5863
Antonio Sonis <anthonysonis@gmail.com>
2024-04-10 11:49:44 +0200

    fix: updating tooltip
    Signed-off-by: Antonio Sonis <anthonysonis@gmail.com>

--- src/renderer/src/components/applications/all/Row.jsx
+++ src/renderer/src/components/applications/all/Row.jsx
@@ -4,7 +4,7 @@ import { useState, useEffect } from 'react'
 import typographyStyles from '~/styles/Typography.module.css'
 import commonStyles from '~/styles/CommonStyles.module.css'
 import tooltipStyles from '~/styles/TooltipStyles.module.css'
-import { DULLS_BACKGROUND_COLOR, ERROR_RED, MEDIUM, RICH_BLACK, SMALL, WARNING_YELLOW, WHITE } from '@platformatic/ui-components/src/components/constants'
+import { DULLS_BACKGROUND_COLOR, ERROR_RED, MEDIUM, POSITION_CENTER, RICH_BLACK, SMALL, WARNING_YELLOW, WHITE } from '@platformatic/ui-components/src/components/constants'
 import { ButtonOnlyIcon, Icons, PlatformaticIcon, TooltipAbsolute } from '@platformatic/ui-components'
 import styles from './Row.module.css'
 import { getFormattedDate } from '~/utilityDetails'
@@ -176,7 +176,16 @@ function Row ({
         <div className={styles.tableCell}>
           <div className={`${styles.customSmallFlexRow}`}>
             <span className={`${typographyStyles.desktopBody} ${typographyStyles.textWhite} ${typographyStyles.opacity70}`}>{platformaticVersion || '-'}</span>
-            {!isLatestPltVersion && platformaticVersion && <Icons.AlertIcon color={WARNING_YELLOW} size={SMALL} />}
+            {!isLatestPltVersion && platformaticVersion && (
+              <TooltipAbsolute
+                tooltipClassName={`${tooltipStyles.tooltipDarkStyle} ${styles.smallMargin}`}
+                content={(<span>There is a new Platformatic Version</span>)}
+                offset={44}
+                position={POSITION_CENTER}
+              >
+                <Icons.AlertIcon color={WARNING_YELLOW} size={SMALL} />
+              </TooltipAbsolute>
+            )}
           </div>
         </div>
       </div>
