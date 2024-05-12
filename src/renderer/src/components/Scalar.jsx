23575e84078869496377e0234986d2c98ebe0745
marcopiraccini <marco.piraccini@gmail.com>
2024-05-12 14:16:24 +0200

    Disable salar sidebar
    Signed-off-by: marcopiraccini <marco.piraccini@gmail.com>

--- src/renderer/src/components/Scalar.jsx
+++ src/renderer/src/components/Scalar.jsx
@@ -18,7 +18,8 @@ export default function Scalar ({ url }) {
         customCss,
         spec: {
           url: `${url}/documentation/json`
-        }
+        },
+        showSidebar: false
       }}
     />
   )
